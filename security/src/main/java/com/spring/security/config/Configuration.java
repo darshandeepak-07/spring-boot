b4871d3022eb5fa91700cc8bb0f232ad8c7ff351
deepak <darshandeepak1483@gmail.com>
2024-12-08 16:33:13 +0530

    impemented jwt initial configs

--- security/src/main/java/com/spring/security/config/Configuration.java
+++ security/src/main/java/com/spring/security/config/Configuration.java
@@ -3,9 +3,11 @@ package com.spring.security.config;
 import com.spring.security.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,7 +26,11 @@ public class Configuration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("register", "login")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
@@ -53,5 +59,10 @@ public class Configuration {
         provider.setPasswordEncoder(new BCryptPasswordEncoder(10));
         provider.setUserDetailsService(userDetailsService);
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- security/src/main/java/com/spring/security/service/UserService.java
+++ security/src/main/java/com/spring/security/service/UserService.java
@@ -3,6 +3,9 @@ package com.spring.security.service;
 import com.spring.security.model.User;
 import com.spring.security.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +15,25 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
 
     public User register(User user) {
         user.setPassword(encoder.encode(user.getPassword()));
         return repository.save(user);
+    }
+
+    public String verifyUser(User user) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated()) {
+            return jwtService.generateToken();
+        } else {
+            return "Login Failed";
+        }
     }
 }
