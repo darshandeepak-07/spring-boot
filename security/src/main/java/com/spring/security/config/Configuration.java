a007aa64846c835c0b1f960a21b580af9584ec21
deepak <darshandeepak1483@gmail.com>
2024-12-08 14:37:24 +0530

    implemented user authentication from database

--- security/src/main/java/com/spring/security/config/Configuration.java
+++ security/src/main/java/com/spring/security/config/Configuration.java
@@ -1,15 +1,23 @@
 package com.spring.security.config;
 
+import com.spring.security.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @org.springframework.context.annotation.Configuration
 @EnableWebSecurity
 public class Configuration {
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,5 +27,30 @@ public class Configuration {
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    // To manually create users without database
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        UserDetails user1 = User
+//                .withDefaultPasswordEncoder()
+//                .username("darshan")
+//                .password("123")
+//                .build();
+//        UserDetails user2 = User
+//                .withDefaultPasswordEncoder()
+//                .username("deepak")
+//                .password("123")
+//                .build();
+//        return new InMemoryUserDetailsManager(user1,user2);
+//    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
     }
 }
---
+++ security/src/main/java/com/spring/security/service/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.spring.security.service;
+
+import com.spring.security.model.User;
+import com.spring.security.model.UserKnown;
+import com.spring.security.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repo.findByUsername(username);
+        if (user == null) {
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new UserKnown(user);
+    }
+}
